a9ba33db9123df2b746cef9e032477f1afb6ac19
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2026-05-29 14:06:29 +0800

    Move `image_path` addition to `create_options_table` migration

--- database/migrations/2026_05_27_043443_create_options_table.php
+++ database/migrations/2026_05_27_043443_create_options_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignUuid('poll_uuid')->constrained('polls')->cascadeOnDelete();
             $table->string('value');
+            $table->string('image_path', 255)->nullable();
             $table->timestamps();
         });
     }
